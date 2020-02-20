e42e454df5704777f3227a9dbb5c81a380fe7e08
Yuriy Ulivanov <vr0rtex@mail.ru>
2020-02-21 00:21:32 +0400

    add: implement typical arrays problem algorithm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return isEmpty(array) ? 0 : array.reduce((current, next) => {
+    return current < next ? current : next;
+  });
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return isEmpty(array) ? 0 : array.reduce((current, next) => {
+    return current > next ? current : next;
+  });
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  return isEmpty(array) ? 0 : array.reduce((current, next) => current + next) / array.length;
+}
+
+function isEmpty(array) {
+  return array == undefined || array.length == 0;
 }
